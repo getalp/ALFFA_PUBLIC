02fa5503a2ddab8d18bbf1650b5f85bc0c886dd4
Elodie Gauthier <elodie.gauth@gmail.com>
2015-01-20 13:25:48 +0100

    kaldi-scripts updated

--- ASR/SWAHILI/kaldi-scripts/04e_train_sgmm.sh
+++ ASR/SWAHILI/kaldi-scripts/04e_train_sgmm.sh
@@ -10,8 +10,8 @@ steps/train_ubm.sh  500 data/train data/lang exp/system1/tri3b_ali exp/system1/u
 steps/train_sgmm2.sh  5000 12000 data/train data/lang exp/system1/tri3b_ali exp/system1/ubm5b2/final.ubm exp/system1/sgmm2_5b2 || exit 1;
 utils/mkgraph.sh data/lang exp/system1/sgmm2_5b2 exp/system1/sgmm2_5b2/graph
 steps/decode_sgmm2.sh --nj 4  --transform-dir exp/system1/tri3b/decode_test exp/system1/sgmm2_5b2/graph data/test exp/system1/sgmm2_5b2/decode_test
-steps/align_sgmm2.sh --nj 20  --transform-dir exp/system1/tri3b_ali  --use-graphs true --use-gselect true data/train data/lang exp/system1/sgmm2_5b2 exp/system1/sgmm2_5b2_ali  || exit 1; 
-steps/make_denlats_sgmm2.sh --nj 20 --sub-split 20  --transform-dir exp/system1/tri3b_ali data/train data/lang exp/system1/sgmm2_5b2_ali exp/system1/sgmm2_5b2_denlats  || exit 1;
+steps/align_sgmm2.sh --nj 4  --transform-dir exp/system1/tri3b_ali  --use-graphs true --use-gselect true data/train data/lang exp/system1/sgmm2_5b2 exp/system1/sgmm2_5b2_ali  || exit 1; 
+steps/make_denlats_sgmm2.sh --nj 4 --sub-split 4  --transform-dir exp/system1/tri3b_ali data/train data/lang exp/system1/sgmm2_5b2_ali exp/system1/sgmm2_5b2_denlats  || exit 1;
 steps/train_mmi_sgmm2.sh  --transform-dir exp/system1/tri3b_ali --boost 0.1 data/train data/lang exp/system1/sgmm2_5b2_ali exp/system1/sgmm2_5b2_denlats exp/system1/sgmm2_5b2_mmi_b0.1  || exit 1;
 
 for iter in 1 2 3 4; do
@@ -32,6 +32,6 @@ local/score_mbr.sh data/test data/lang exp/system1/sgmm2_5b2_mmi_b0.1/decode_tes
 
 # SGMM+MMI+fMMI
 
-local/score_combine.sh data/test data/lang exp/system1/tri3b_fmmi_indirect/decode_test_it3  exp/system1/sgmm2_5b_mmi_b0.1/decode_test_it3 exp/system1/combine_tri3b_fmmi_indirect_sgmm2_5b_mmi_b0.1/decode_test_it8_3
+local/score_combine.sh data/test data/lang exp/system1/tri3b_fmmi_indirect/decode_test_it3  exp/system1/sgmm2_5b2_mmi_b0.1/decode_test_it3 exp/system1/combine_tri3b_fmmi_indirect_sgmm2_5b2_mmi_b0.1/decode_test_it8_3
 
 echo -e "SGMM training done.\n"
