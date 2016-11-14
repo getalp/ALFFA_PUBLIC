28ec2d77c90ba3907f82b8f2e16494fe929acd7b
Elodie Gauthier <elodie.gauth@gmail.com>
2016-11-14 09:59:02 +0100

    Some improvements on scripts

--- ASR/FONGBE/kaldi-scripts/00_init_paths.sh
+++ ASR/FONGBE/kaldi-scripts/00_init_paths.sh
@@ -12,7 +12,7 @@ LEXICON=$DATA_DIR/local/dict/lexicon.txt
 # Put the path mapping to your kaldi installation
 KALDI_DIR=$HOME/kaldi-trunk
 
-PATH=$PATH:./:$KALDI_DIR/src/bin:$KALDI_DIR/src/gmmbin:$KALDI_DIR/src/latbin:$KALDI_DIR/src/featbin:$KALDI_DIR/tools/openfst/bin:$KALDI_DIR/src/fstbin:$WORK_DIR/utils:$WORK_DIR/steps:$WORK_DIR/utils:$KALDI_DIR/src/sgmmbin/:$KALDI_DIR/src/fgmmbin:$KALDI_DIR/src/sgmm2bin:$KALDI_DIR/src/nnet-cpubin/:
+PATH=$PATH:./:$KALDI_DIR/src/bin:$KALDI_DIR/src/lmbin:$KALDI_DIR/src/gmmbin:$KALDI_DIR/src/latbin:$KALDI_DIR/src/featbin:$KALDI_DIR/tools/openfst/bin:$KALDI_DIR/src/fstbin:$WORK_DIR/utils:$WORK_DIR/steps:$WORK_DIR/utils:$KALDI_DIR/src/sgmmbin/:$KALDI_DIR/src/fgmmbin:$KALDI_DIR/src/sgmm2bin:$KALDI_DIR/src/nnet-cpubin/:
 
 export PATH
 export LC_ALL=C
--- ASR/FONGBE/kaldi-scripts/03_lm_preparation.sh
+++ ASR/FONGBE/kaldi-scripts/03_lm_preparation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-. ./kaldi-scripts/00_init_paths.sh  || die "00_init_paths.sh expected";
+. ./kaldi-scripts/00_init_paths.sh  || { echo -e "\n00_init_paths.sh expected.\n"; exit; } 
 
 #We use a LM with %PPL=164 
 #convert to FST format for Kaldi
@@ -9,11 +9,11 @@ cat ./LM/fongbe.arpa |    \
     grep -v '<s> <s>' | \
     grep -v '</s> <s>' | \
     grep -v '</s> </s>' | \
-    $KALDI_DIR/src/bin/arpa2fst - | $KALDI_DIR/tools/openfst-1.3.4/bin/fstprint | \
+    arpa2fst - | fstprint | \
     utils/remove_oovs.pl LM/oovs.txt | \
-    utils/eps2disambig.pl | utils/s2eps.pl | $KALDI_DIR/tools/openfst-1.3.4/bin/fstcompile --isymbols=data/lang/words.txt \
+    utils/eps2disambig.pl | utils/s2eps.pl | fstcompile --isymbols=data/lang/words.txt \
       --osymbols=data/lang/words.txt  --keep_isymbols=false --keep_osymbols=false | \
-     $KALDI_DIR/tools/openfst-1.3.4/bin/fstrmepsilon > ./data/lang/G.fst
+     fstrmepsilon > ./data/lang/G.fst
 
 #if prep_lang.sh returns G.fst is not ilabel sorted, run this to sort
 fstarcsort --sort_type=ilabel data/lang/G.fst > data/lang/G_new.fst
