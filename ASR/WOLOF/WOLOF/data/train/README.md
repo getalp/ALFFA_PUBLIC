005fdba73e09a533c75e0b8e942f5d78cdf358fb
besacier <laurent.besacier@imag.fr>
2017-07-18 22:11:29 +0200

    put back WOLOF data

--- ASR/WOLOF/WOLOF/data/train/README.md
+++
@@ -1,6 +0,0 @@
-### Description of the file list          
-          
-#### Directory: train/          
-+ **wav/ **: directories containing the WAV files of the 14 speakers used for training          
-+ **text**: original transcript file formatted for Kaldi use          
-+ **metadata_train.txt**: some info about the training set          
--- ASR/WOLOF/WOLOF/kaldi-scripts/01_init_symlink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. ./kaldi-scripts/00_init_paths.sh || die "00_init_paths.sh expected";
-
-if [ ! -L utils ]; then ln -s $KALDI_DIR/egs/wsj/s5/utils; fi
-if [ ! -L conf ]; then ln -s $KALDI_DIR/egs/wsj/s5/conf; fi
-if [ ! -L steps ]; then ln -s $KALDI_DIR/egs/wsj/s5/steps; fi
-if [ ! -L local ]; then ln -s $KALDI_DIR/egs/wsj/s5/local; fi
-if [ ! -L cmd.sh ]; then ln -s $KALDI_DIR/egs/wsj/s5/cmd.sh; fi
